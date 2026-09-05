3df61129f3dbd2cc356f839659ccb8f02884c986
LoOoM8485 <elmu@hotmail.it>
2026-09-05 14:12:43 +0900

    Make Invitational Firestore resilient across tablet reconnects

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {
+  initializeFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGU-ZKIQEdk6M37Jt65S-tMydQsoaswh4",
@@ -8,9 +12,13 @@ const firebaseConfig = {
   storageBucket: "tableplanner123.firebasestorage.app",
   messagingSenderId: "78386456966",
   appId: "1:78386456966:web:1aee4ea19d1d91f3f2e407",
-  measurementId: "G-8LJRBDWS6B"
+  measurementId: "G-8LJRBDWS6B",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentMultipleTabManager(),
+  }),
+});
